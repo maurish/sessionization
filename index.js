4ee17d70ffe99d4f27526b61713865d0a98abbb7
Pauli Perälä <pauli.perala@nitor.fi>
2018-05-22 00:01:59 +0300

    write in stdout

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const fs = require('fs');
 const sessionizer = require('./src/sessionizer');
 
 const run = async (stream) => {
   const response = await sessionizer.sessionize(stream);
-  fs.writeFile('result.json', JSON.stringify(response, null, 2), (err, res) => {})
+  response
+    .map(record => JSON.stringify(record, null, 2) + '\n')
+    .forEach(string => process.stdout.write(string));
 }
 
 run(process.stdin);
